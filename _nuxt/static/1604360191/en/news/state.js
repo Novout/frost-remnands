13ec190e4e937760258b000dc9b4124230233a0f
Novout <novout@hotmail.com>
2020-11-02 23:36:44 +0000

    Deploying to gh-pages from  @ bfa053aa426ebb2cb433225785c47fb81f520d0d 🚀

---
+++ _nuxt/static/1604360191/en/news/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1604360191",layout:"default",error:null,serverRendered:a,routePath:"\u002Fen\u002Fnews",config:{},ssrRefs:{},colorMode:{preference:b,value:b,unknown:a,forced:false}}}(true,"system"));
