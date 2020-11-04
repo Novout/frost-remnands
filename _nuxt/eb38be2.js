e560a39960b2450358959411178cbd78d7c40920
Novout <novout@hotmail.com>
2020-11-04 17:11:09 +0000

    Deploying to gh-pages from  @ fd4182ed729057d37004fd00691e35ceff1b6ef2 🚀

---
+++ _nuxt/eb38be2.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{292:function(e,t,n){"use strict";n.r(t);var l=n(17),c=Object(l.defineComponent)({head:function(){return{title:this.$i18n.t("default.head.news")}}}),o=n(14),component=Object(o.a)(c,(function(){var e=this.$createElement;return(this._self._c||e)("p",[this._v("Test")])}),[],!1,null,null,null);t.default=component.exports}}]);
